0c6bfb21cd880c8a625bccb0211c32ab5300449b
kertser <kertser@yahoo.com>
2024-02-07 11:32:47 +0200

    Refining structure

---
+++ include/RZ_300_HDR.h
@@ -0,0 +1,11 @@
+//
+// Created by kerts on 2/07/2024.
+//
+
+#ifndef APICALCULATOR_RZ_300_HDR_H
+#define APICALCULATOR_RZ_300_HDR_H
+
+// RED for RZ-104-11
+double RED_RZ_300_HDR(double Flow, double UVT, double P, double Eff, double D1Log, uint32_t NLamps);
+
+#endif //APICALCULATOR_RZ_300_HDR_H
---
+++ libs/RZ_300_HDR.c
@@ -0,0 +1,53 @@
+//
+// Created by kerts on 2/07/2024.
+// RZ-104-11 RED calculation
+//
+
+//#include "RZ_300_HDR.h"
+#include <math.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "utils.h"
+
+// RED for RZ-300 (HydraQual algorithm)
+double RED_RZ_300_HDR(double Flow, double UVT, double P, double Eff, double D1Log, uint32_t NLamps) {
+    // The function is for HDR validation model
+
+    /*
+    #General scheme for HDR calculator:
+
+    RED = 10a * (P/100)b * (Q)c * (1/Abs)d * * (UVS)e * 10(f/abs) * Ng
+    abs = -LOG(UVT/100)
+
+    UVS = D1Log average inactivation
+    N = number of lamps
+    Flow units - [m3/h]
+    */
+
+    bool legacy = false; // legacy RED function
+
+    // Dose Coefficents:
+    const double a = 1.9723460342;
+    const double b = 1.1342181946;
+    const double c = -0.89447843101;
+    const double d = 1.6521724013;
+    const double e = 0.1018468648;
+    const double f = -0.0060928379303;
+    const double g = 1.1855262222;
+
+    const double VF_A = 1.006642717; // VF calculation
+    const double VF_B = 6.181224702; // VF calculation
+
+    const double LampPower = 4200; // [W]
+    const double minFlow = 30; //[m^3h]
+    const double maxFlow = 500; //[m^3h]
+
+    double RED;
+    if (NLamps>2)
+        RED = pow(10,a) * pow((P / 100 * Eff / 100) , b) * pow((Flow / 0.2271) , c) * pow((1 / (-log10(UVT / 100))) , d) * pow(
+                D1Log , e) * pow(10 , (f / (-log10(UVT / 100)))) * (NLamps / 2 * pow(2 , g));
+    else
+        RED = pow(10,a) * pow((P / 100 * Eff / 100) , b) * pow((Flow / 0.2271) , c) * pow((1 / (-log10(UVT / 100))) , d) * pow(
+                D1Log , e) * pow(10 , (f / (-log10(UVT / 100)))) * pow(NLamps , g);
+    return RED;
+}
